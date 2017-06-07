41e0f836f18926e62bbae24fffb05ff5926b31f1
daewonlee <lee.daewon@gmail.com>
2017-06-07 04:21:27 -0400

    add comments

--- main.m
+++ main.m
@@ -1,43 +1,53 @@
-clear all
-close all
+%   Created on: June 6, 2017
+%   Author: Daewon Lee
+%   email: lee.daewon@gmail.com
 
-obs1 = [5,3 ; 8,3; 8,12; 3,12; 3,7; 5,7];
+%% Setting
+clear all; close all 
 
-start = [1,1];
-goal = [12,12];
-costmap_init = ones(12)*999;
-isSearchedMap = zeros(size(costmap_init));
-mapHeuristic = findHeuristicMap(size(costmap_init),goal);
+obs1 = [5,3 ; 8,3; 8,12; 3,12; 3,7; 5,7]; % obstacle polygon
+obs2 = [10,9; 10,12; 11,12; 11,9];
+
+start = [1,11]; % start position
+goal = [12,12]; % goal 
+initialCost = 999;
+costmap_init = ones(12)*initialCost;  % creat map (12x12) , initialCost should be bigger than possible cost in the map
+mapSize = size(costmap_init);
+isSearchedMap = zeros(size(costmap_init)); % 0: not visited node, 1: visited node
+mapHeuristic = findHeuristicMap(size(costmap_init),goal); % heuristic map with euclidian distance
 searchList = [];
 
+costmap=occupyObsMap(costmap_init,obs1); % occupy the map with obstacle
+costmap=occupyObsMap(costmap,obs2);
 
-costmap=occupyObsMap(costmap_init,obs1);
-
-initVal = findDistance(start,goal);
+%% Initialization
+initVal = findDistance(start,goal); 
 isSearchedMap(start)=1;
 costmap(start) = initVal;
-node=[1,initVal,start(1),start(2),0,0]; % [ID, Val, x, y, ID_prev,  cost_prev]
+node=[1,initVal,start(1),start(2),0,0]; % [ID, cost, x, y, ID_prev,  cost from start position to this position]
 [costmap_update, searchListNew] = updateCostMap( costmap, mapHeuristic, isSearchedMap, node);
 costmap = costmap_update;
-searchList=[searchList;searchListNew];
+searchList=[searchList;searchListNew]; % [cost, x, y, node ID, cost from start position to this position]
 
+%% Main Loop
 while(1)
 
-    [r,c]=find(searchList==min(searchList(:,1)));
-    node = [node; size(node,1)+1, searchList(r(1),1), searchList(r(1),2), searchList(r(1),3), searchList(r(1),4),  searchList(r(1),5)];
-    isSearchedMap(searchList(r(1),2), searchList(r(1),3)) = 1;
+    [r,c]=find(searchList==min(searchList(:,1))); % find an index (r) of a not-visited node that has lowest cost 
+    node = [node; size(node,1)+1, searchList(r(1),1), searchList(r(1),2), searchList(r(1),3), searchList(r(1),4),  searchList(r(1),5)]; % update node
+    isSearchedMap(searchList(r(1),2), searchList(r(1),3)) = 1; % update isSearchedMap
     searchList(r(1),:)=[];
 
-    if((node(size(node,1),3) == 12) && (node(size(node,1),4) == 12))
+    if((node(size(node,1),3) == mapSize(1)) && (node(size(node,1),4) == mapSize(2)))
         break;
     end
     [costmap_update, searchListNew] = updateCostMap( costmap, mapHeuristic, isSearchedMap, node(size(node,1),:));
     costmap = costmap_update;
-    searchList=[searchList;searchListNew];
+    searchList=[searchList;searchListNew]; % update searchList
 
 end
 
-path = [node(size(node,1),3),node(size(node,1),4)];
+%% Find Optimal Path
+path = [node(size(node,1),3),node(size(node,1),4)]; 
 index_next = node(size(node,1),5);
 while(1)
     path=[node(index_next,3),node(index_next,4);path];
@@ -47,10 +57,12 @@ while(1)
     end
 end
 
+%% Plot
 figure(1)
 fill(obs1(:,1), obs1(:,2), 'r');
 hold on
+fill(obs2(:,1), obs2(:,2), 'r');
 plot(start(1),start(2),'o');
 plot(goal(1),goal(2),'*');
 plot(path(:,1),path(:,2),'x')
-
+axis([0 mapSize(1) 0 mapSize(2)])
--- updateCostMap.m
+++ updateCostMap.m
@@ -1,6 +1,7 @@
+%   Created on: June 6, 2017
+%   Author: Daewon Lee
+%   email: lee.daewon@gmail.com
 function [costmap_update, search_list_new] = updateCostMap( costmap, mapHeuristic, isSearchedMap, node)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
     costmap_update = costmap;
     isSearchedMap_update = isSearchedMap;
     new_node = [];
@@ -9,11 +10,11 @@ function [costmap_update, search_list_new] = updateCostMap( costmap, mapHeuristi
     p = [node(3),node(4)];
     for i=-1:1
         for j=-1:1
-           if ((p(1) + i > 0) && (p(2) + j > 0) && (p(1) + i <= size(costmap,1)) && (p(2) + j  <= size(costmap,2)))
-                if (( isSearchedMap(p(1)+i, p(2)+j) == 0) && costmap(p(1)+i, p(2)+j) ~= 1000)
-                    cost_prev = node(6) + findDistance([p(1)+i, p(2)+j], [p(1), p(2)]);
-                    value = cost_prev + mapHeuristic(p(1)+i, p(2)+j);
-                    if(costmap(p(1)+i, p(2)+j) > value)
+           if ((p(1) + i > 0) && (p(2) + j > 0) && (p(1) + i <= size(costmap,1)) && (p(2) + j  <= size(costmap,2))) % check if the node is inside of the map
+                if (( isSearchedMap(p(1)+i, p(2)+j) == 0) && costmap(p(1)+i, p(2)+j) ~= 1000) % 1000 : obstacle
+                    cost_prev = node(6) + findDistance([p(1)+i, p(2)+j], [p(1), p(2)]); % node(6) : cost from start position to this position
+                    value = cost_prev + mapHeuristic(p(1)+i, p(2)+j); % total cost
+                    if(costmap(p(1)+i, p(2)+j) > value) % update the cost map & searchList only if the new cost is lower than previous cost
                         costmap_update(p(1)+i, p(2)+j) = value;
                         search_list_new = [search_list_new; value, p(1)+i, p(2)+j, node(1), cost_prev];     
                     end
